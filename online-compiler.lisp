e4b9f5e6e5e7855871415f5308f8e874be3d3fa6
Zachary Taylor <35826908+zlokquay@users.noreply.github.com>
2019-04-10 17:20:07 -0400

    Create online-compiler.lisp
    Just accepts the variables, without checking for the string.
    Has slight error handling for too big numbers. Prints the default board state.

---
+++ online-compiler.lisp
@@ -0,0 +1,51 @@
+(princ "Enter the size of the board: ")
+(defvar *size* (read))
+(terpri)
+(write *size*)
+(terpri)
+
+(princ "Enter the number of live neighbors for a live cell to stay alive: ")
+(defvar *steady-neighbors* (read))
+(terpri)
+(write *steady-neighbors*)
+(terpri)
+
+(princ "Enter the number of live neighbors for a dead cell to become alive: ")
+(defvar *necro-neighbors* (read))
+(terpri)
+(write *necro-neighbors*)
+(terpri)
+
+(princ "Enter the distance for the neighbor rule: ")
+(loop
+    (setq *neighbors-distance* (read))
+    (terpri)
+    (write *neighbors-distance*)
+    (terpri)
+    (if (< *neighbors-distance* *size*)
+        (return *neighbors-distance*)
+    )
+    (princ "ERROR: Neighbor distance bigger than board size; re-enter: ")
+)
+
+(princ "Enter number of live tiles to start with: ")
+(loop
+    (setq *starting-tiles* (read))
+    (terpri)
+    (write *starting-tiles*)
+    (terpri)
+    (if (< *starting-tiles* (* *size* *size*))
+        (return *starting-tiles*)
+    )
+    (princ "ERROR: More live tiles than board size; re-enter: ")
+)
+
+(setq *board* (make-array (list *size* *size*)))
+
+(dotimes (i *size*)
+    (dotimes (j *size*)
+        (setf (aref *board* i j) "d")
+    )
+)
+
+(princ *board*)
